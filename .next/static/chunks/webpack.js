438b1e774965bb6a52306797992562c93e7d0ba7
Kingvik7 <60291701+Kingvik7@users.noreply.github.com>
2023-04-30 22:30:25 +0100

    Was bored so made the logo anim

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fb85bd83dddbcd1c"; }
+/******/ 		__webpack_require__.h = function() { return "e32dd5c117671a0c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/TodoList.tsx
+++ pages/TodoList.tsx
@@ -35,6 +35,7 @@ import { gsap } from "gsap"
     useEffect(() => {
       const list = listRef.current;
       const newestItem = list.lastChild;
+
       if (newestItem) {
         gsap.from(newestItem, {
           opacity: 0,
@@ -42,10 +43,11 @@ import { gsap } from "gsap"
           ease: "power1.inOut",
         });
       }
+  
     }, [todos]);
 
   return (
-    <div>
+    <div className="list">
       <p className="text-white mb-2 text-3xl mt-28 ">Your todos</p>
       <p className="text-gray-500 mt-4 text-sm">Pending</p>
       <ul className="mt-1 list" ref={listRef}>
@@ -53,20 +55,20 @@ import { gsap } from "gsap"
           <li key={index} className="flex my-2 todo-item ">
           <input
             type="checkbox"
-            className="checkbox-custom" 
+            className="checkbox-custom hover:bg-yellow-300 opacity-80 transition-all" 
             id={`checkbox-${index}`}
             onClick={() => handleCompleteTodo(index)}
           />
           <label htmlFor={`checkbox-${index}`} className="checkbox-label w-auto">
             {todo}
           </label>
-          <button className="text-end pl-4" onClick={() => handleDeleteTodo(index)}>
+          <button className="ml-auto pl-4" onClick={() => handleDeleteTodo(index)}>
           <svg className="trash" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M2 4H3.33333H14"  stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M12.6667 3.99999V13.3333C12.6667 13.6869 12.5262 14.0261 12.2761 14.2761C12.0261 14.5262 11.6869 14.6667 11.3333 14.6667H4.66666C4.31304 14.6667 3.9739 14.5262 3.72385 14.2761C3.4738 14.0261 3.33333 13.6869 3.33333 13.3333V3.99999M5.33333 3.99999V2.66666C5.33333 2.31304 5.4738 1.9739 5.72385 1.72385C5.9739 1.4738 6.31304 1.33333 6.66666 1.33333H9.33333C9.68695 1.33333 10.0261 1.4738 10.2761 1.72385C10.5262 1.9739 10.6667 2.31304 10.6667 2.66666V3.99999"  stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M6.66667 7.33334V11.3333"  stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M9.33333 7.33334V11.3333"  stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
+            <path d="M2 4H3.33333H14"  stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M12.6667 3.99999V13.3333C12.6667 13.6869 12.5262 14.0261 12.2761 14.2761C12.0261 14.5262 11.6869 14.6667 11.3333 14.6667H4.66666C4.31304 14.6667 3.9739 14.5262 3.72385 14.2761C3.4738 14.0261 3.33333 13.6869 3.33333 13.3333V3.99999M5.33333 3.99999V2.66666C5.33333 2.31304 5.4738 1.9739 5.72385 1.72385C5.9739 1.4738 6.31304 1.33333 6.66666 1.33333H9.33333C9.68695 1.33333 10.0261 1.4738 10.2761 1.72385C10.5262 1.9739 10.6667 2.31304 10.6667 2.66666V3.99999"  stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M6.66667 7.33334V11.3333"  stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M9.33333 7.33334V11.3333"  stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
  
 </button>
         </li>
@@ -74,32 +76,33 @@ import { gsap } from "gsap"
       </ul>
       <p className="text-gray-500 mt-4 text-sm">Completed</p>
       
-      <ul>
+      <ul className="list" >
         {completedTodos.map((completedTodo, index) => (
-            <div className="flex flex-row my-2 todo-item2">
+            <div className="flex flex-row my-2 todo-item2 fade-in">
                 <svg className="checkbox-custom-completed opacity-25" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect width="14" height="14" rx="2" fill="#FFE800"/>
-<path d="M3 7.6L5.54545 10L10 4" stroke="#222222"/>
-</svg>
+                  <rect width="14" height="14" rx="2" fill="#FFE800"/>
+                  <path d="M3 7.6L5.54545 10L10 4" stroke="#222222"/>
+                </svg>
 
-          <li className="text-zinc-600 line-through w-auto" key={index}>{completedTodo}</li>
+          <li className="text-zinc-600   w-auto" key={index}>{completedTodo}</li>
    
 
-          <button className="text-end pl-4" onClick={() => handleDeleteCompletedTask(index)}><svg className="trash" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M2 4H3.33333H14"  stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M12.6667 3.99999V13.3333C12.6667 13.6869 12.5262 14.0261 12.2761 14.2761C12.0261 14.5262 11.6869 14.6667 11.3333 14.6667H4.66666C4.31304 14.6667 3.9739 14.5262 3.72385 14.2761C3.4738 14.0261 3.33333 13.6869 3.33333 13.3333V3.99999M5.33333 3.99999V2.66666C5.33333 2.31304 5.4738 1.9739 5.72385 1.72385C5.9739 1.4738 6.31304 1.33333 6.66666 1.33333H9.33333C9.68695 1.33333 10.0261 1.4738 10.2761 1.72385C10.5262 1.9739 10.6667 2.31304 10.6667 2.66666V3.99999"  stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M6.66667 7.33334V11.3333"  stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M9.33333 7.33334V11.3333"  stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-</button>
+          <button className="ml-auto pl-4" onClick={() => handleDeleteCompletedTask(index)}>
+            <svg className="trash" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2 4H3.33333H14"  stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M12.6667 3.99999V13.3333C12.6667 13.6869 12.5262 14.0261 12.2761 14.2761C12.0261 14.5262 11.6869 14.6667 11.3333 14.6667H4.66666C4.31304 14.6667 3.9739 14.5262 3.72385 14.2761C3.4738 14.0261 3.33333 13.6869 3.33333 13.3333V3.99999M5.33333 3.99999V2.66666C5.33333 2.31304 5.4738 1.9739 5.72385 1.72385C5.9739 1.4738 6.31304 1.33333 6.66666 1.33333H9.33333C9.68695 1.33333 10.0261 1.4738 10.2761 1.72385C10.5262 1.9739 10.6667 2.31304 10.6667 2.66666V3.99999"  stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M6.66667 7.33334V11.3333"  stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M9.33333 7.33334V11.3333"  stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </button>
           </div>
         ))}
       </ul>
       <p className="text-white mb-2 text-3xl mt-28 ">New todo</p> 
       <form onSubmit={handleAddTodo}>
-        <div className="rounded-sm overflow-hidden">
-            <input className=" p-1 text-gray-500" type="text" name="todo" ref={inputRef} placeholder="Add a to-do" />
-            <button className="bg-yellow-300 p-1 px-3 rounded-r-sm text-gray-500" type="submit"><p className="hover:rotate-90 transition-all text-md">+</p>
+        <div className="rounded-md overflow-hidden">
+            <input className=" p-1 px-2 h-10 text-gray-500" type="text" name="todo" ref={inputRef} placeholder="Add a to-do" />
+            <button className="bg-yellow-300 w-10 rounded-r-md h-10 hover:opacity-80 transition-opacity text-gray-500" type="submit"><p className=" text-md">+</p>
             </button>
         </div>
       </form>
